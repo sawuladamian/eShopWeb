fb5b4b6abf78f4dae5d213fae057673c3f71b117
Damian Sawuła <damian.sawula@gmail.com>
2022-03-30 12:11:31 +0200

    Fix: Searching items, added bootstrap icons

--- eShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ eShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -21,12 +21,16 @@ namespace eShopWeb.Controllers
        
         public IActionResult Index([FromQuery] string FilterName = "")
         {
+            
             HomeVM vm = new()
             {
-                FilterName = FilterName,
-                Tshirt = _unitOfWork.Tshirt.GetAll(x => x.Name.Contains(FilterName), includeProperties: "Color,Size")
+                FilterName = FilterName,               
+                Tshirt = FilterName!=null ? 
+                _unitOfWork.Tshirt.GetAll(x => x.Name.Contains(FilterName), includeProperties: "Color,Size")  :
+                _unitOfWork.Tshirt.GetAll(includeProperties: "Color,Size")
             };
-            return View(vm);
+            
+           return View(vm);
         }
 
         //public HomeVM GetByName([FromQuery]string FilterName="")
